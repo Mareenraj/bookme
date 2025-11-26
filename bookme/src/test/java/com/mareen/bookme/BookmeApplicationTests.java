cca2a2101d8eb64d0d4179972afe5280ef8655a8
Mareenraj <mareenraj7v2@gmail.com>
2025-11-26 22:30:24 +0530

    start initial bakckend project

---
+++ bookme/src/test/java/com/mareen/bookme/BookmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mareen.bookme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
